7fac4fdc81bc6a95d89bc4bcca598f19baabd46e
Michel Gutlich <mragutlich@gmail.com>
2025-11-24 21:54:15 +0100

    Clean up

--- PicoBox/PicoBox-DualCore/calculation.ino
+++
@@ -1,9 +0,0 @@
-void doCalculation(){
-  pgnumber = 100 * number[0] + 10 * number[1] + number[2];
-  if (pgnumber > 127){
-    pgnumber = 127;
-    number[0] = 1;
-    number[1] = 2;
-    number[2] = 7;
-  }
-}
